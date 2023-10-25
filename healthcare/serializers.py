0a843563f357a52e49fa77551bbf14c99685ad4f
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-26 01:37:51 +0530

    Removed unnecessary parameters from view. They are there but I am not including them into JSON when sending to the frontend.

--- healthcare/serializers.py
+++ healthcare/serializers.py
@@ -1,15 +1,38 @@
 from rest_framework import serializers
 from .models import Doctor, Patient
 
+
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = '__all__'
+        exclude = [
+            "password",
+            "type",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "groups",
+            "user_permissions",
+            "last_login",
+            "is_superuser",
+            "id"
+        ]
         depth = 1
+
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = '__all__'
+        exclude = [
+            "password",
+            "type",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "groups",
+            "user_permissions",
+            "last_login",
+            "is_superuser",
+            "id"
+        ]
         delth = 1
-
