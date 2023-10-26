13911cb2924dfefea2172be59a14c5ba53a7dc8f
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-26 11:20:38 +0530

    JWT authentication done.

---
+++ healthcare/healthcare_api/views.py
@@ -0,0 +1,26 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+def get_Routes(request):
+    routes = [
+        'healthcare_api/token/',
+        'healthcare_api/token/refresh/'
+    ]
+    return Response(routes)
