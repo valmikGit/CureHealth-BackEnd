08de0e0057d4559af1a1e61acce7eec2c21f37f8
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-26 15:07:36 +0530

    PUT PATCH DELETE GET POST added to patients and doctors

--- healthcare/views.py
+++ healthcare/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 # Create your views here.
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def patients(request):
     if request.method == 'GET':
         patients = Patient.objects.all()
@@ -20,8 +20,36 @@ def patients(request):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+    
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = PatientSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == 'PATCH':
+        data = request.data
+        obj = Patient.objects.get(id=data['id'])
+        serializer = PatientSerializer(obj, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == 'DELETE':
+        data = request.data
+        try:
+            obj = Patient.objects.get(id=data['id'])
+            name = obj.username
+            obj.delete()
+            return Response({'message' : f"{name} deleted successfully."})
+        except Exception as e:
+            return Response({'message' : f"Error is {e}"})
 
-@api_view(['GET', 'POST'])
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def doctors(request):
     if request.method == 'GET':
         patients = Doctor.objects.all()
@@ -35,6 +63,33 @@ def doctors(request):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+    
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = DoctorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == 'PATCH':
+        data = request.data
+        obj = Doctor.objects.get(id=data['id'])
+        serializer = DoctorSerializer(obj, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    elif request.method == 'DELETE':
+        data = request.data
+        try:
+            obj = Doctor.objects.get(id=data['id'])
+            name = obj.username
+            obj.delete()
+            return Response({'message' : f"{name} deleted successfully."})
+        except Exception as e:
+            return Response({'message' : f"Error is {e}"})
     
 def home(request):
     return HttpResponse("<h1>HOME<h1>")
