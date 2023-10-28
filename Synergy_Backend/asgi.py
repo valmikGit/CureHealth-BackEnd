5b8908eda6f185c6ab6124702ef962827e1c06a3
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-29 02:55:26 +0530

    Added chatting app. Not completed that yet.

--- Synergy_Backend/asgi.py
+++ Synergy_Backend/asgi.py
@@ -7,10 +7,54 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Synergy_Backend.settings')
+
+# application = get_asgi_application()
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.urls import path
+# from chatApp.consumers import ChatConsumer
+# from channels.auth import AuthMiddlewareStack
+# from chatApp.routing import websocket_urlpatterns
+
+
+# ws_pattern = [
+#     path('chat/<room_code>', ChatConsumer.as_asgi())
+# ]
+
+# # application = ProtocolTypeRouter(
+# #     {
+# #         'websocket' : (URLRouter(ws_pattern))
+# #     }
+# # )
+
+# application = ProtocolTypeRouter(
+#     {
+#         'websocket' : AuthMiddlewareStack(
+#             URLRouter(websocket_urlpatterns)
+#         )
+#     }
+# )
+
+
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatApp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Synergy_Backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            chatApp.routing.websocket_urlpatterns
+        )
+    )
+})
