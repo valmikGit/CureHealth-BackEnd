3ca65d3ed8dc462ce24c7513d76e414b723de06d
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-30 00:48:11 +0530

    Trying to debug chatApp.

--- healthcare/migrations/0009_alter_appointment_meeting_date_time.py
+++ healthcare/migrations/0009_alter_appointment_meeting_date_time.py
@@ -7,13 +7,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('healthcare', '0007_alter_appointment_meeting_date_time'),
+        ('healthcare', '0008_alter_appointment_meeting_date_time'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='appointment',
             name='meeting_Date_Time',
-            field=models.DateTimeField(default=datetime.datetime(2023, 10, 29, 13, 47, 15, 829709, tzinfo=datetime.timezone.utc), verbose_name='Meeting Date and Time'),
+            field=models.DateTimeField(default=datetime.datetime(2023, 10, 29, 13, 47, 50, 822986, tzinfo=datetime.timezone.utc), verbose_name='Meeting Date and Time'),
         ),
     ]
