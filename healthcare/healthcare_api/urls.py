91b6f17291f841fba6fc2a333da8c412969018de
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-29 16:03:02 +0530

    Modified JWT Authentication a bit. Added appointments model to the admin page.

--- healthcare/healthcare_api/urls.py
+++ healthcare/healthcare_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from healthcare.healthcare_api import views
 from rest_framework.authtoken import views as auth_Token_Views
-from healthcare.healthcare_api.views import MyTokenObtainPairView
+# from healthcare.healthcare_api.views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenObtainPairView,
@@ -10,7 +10,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', view=views.get_Routes),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('notes/', view=views.get_Notes),
