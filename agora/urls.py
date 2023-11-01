f272660aee0eb5e0581a7839992b0093968c0bf8
valmikGit <quantumcomputingiiitb@gmail.com>
2023-11-02 01:31:21 +0530

    Minor edit.

--- agora/urls.py
+++ agora/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from agora import views
 urlpatterns = [
-    path('', view=views.get_Agora_Routes, name="Routes possible from here"),
+    path('', view=views.get_Agora_Routes),
     path('index/', views.index, name='agora-index'),
     path('pusher/auth/', views.pusher_auth, name='agora-pusher-auth'),
     path('token/', views.generate_agora_token, name='agora-token'),
