295e87a3094fc8539e7fe044dbca836ea52e3309
valmikGit <quantumcomputingiiitb@gmail.com>
2023-11-14 12:53:16 +0530

    Tried to remove the bug in password hashing when trying to register a new user.

--- Synergy_Backend/settings.py
+++ Synergy_Backend/settings.py
@@ -215,3 +215,11 @@ CACHES = {
         }
     }
 }
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',  # Not recommended for production
+]
